e90dd3dfe0b2c67fe51e4f9fc66680893ba58f85
hideki-okawa <h.okawa0303@gmail.com>
2023-09-18 15:52:54 +0900

    fix style

--- popup/components/header/index.tsx
+++ popup/components/header/index.tsx
@@ -2,7 +2,7 @@ import { BiLogoGithub as GitHubIcon } from "react-icons/bi"
 
 const Header = () => {
   return (
-    <header className="h-12 text-lg text-white bg-black p-2 flex items-center gap-2">
+    <header className="h-12 text-lg text-white bg-black px-3 py-2 flex items-center gap-2">
       <GitHubIcon />
       <span>GitHub Compare Generator</span>
     </header>
