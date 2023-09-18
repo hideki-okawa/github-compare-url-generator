8af657d0a26cb8e63aa0cba2db9454358fbc679e
hideki-okawa <h.okawa0303@gmail.com>
2023-09-18 15:52:37 +0900

    create button component

--- popup/components/body/button.tsx
+++ popup/components/body/button.tsx
@@ -1,5 +1,21 @@
-const Button = () => {
-  return <button>Click me</button>
+import { on } from "events"
+
+type buttonProps = {
+  title: string
+  onClick: () => void
+}
+
+const Button = ({ title, onClick }: buttonProps) => {
+  return (
+    <button
+      type="button"
+      className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-normal rounded-lg text-sm px-5 py-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      onClick={() => {
+        onClick()
+      }}>
+      {title}
+    </button>
+  )
 }
 
 export default Button
--- popup/components/body/index.tsx
+++ popup/components/body/index.tsx
@@ -9,19 +9,35 @@ type bodyProps = {
 }
 
 const Body = ({ repositoryURL }: bodyProps) => {
-  const [compareURL, setCompareURL] = useState<String>("")
-  const [beforeCommitHash, setBeforeCommitHash] = useState<String>("")
-  const [afterCommitHash, setAfterCommitHash] = useState<String>("")
+  const [compareURL, setCompareURL] = useState("")
+  const [beforeCommitHash, setBeforeCommitHash] = useState("")
+  const [afterCommitHash, setAfterCommitHash] = useState("")
+  const [successMessage, setSuccessMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
 
   // GitHubのハッシュが変更されたときにURLを更新する
   useEffect(() => {
     setCompareURL(
       `${repositoryURL}/compare/${beforeCommitHash}...${afterCommitHash}`
     )
-  }, [beforeCommitHash, afterCommitHash])
+  }, [repositoryURL, beforeCommitHash, afterCommitHash])
+
+  const compareURLCopyToClipboard = () => {
+    try {
+      navigator.clipboard.writeText(compareURL)
+      setSuccessMessage("Text copied to clipboard!!")
+    } catch (err) {
+      setErrorMessage(`error: ${err}`)
+    }
+  }
+
+  const compareURLOpenToNewTab = () => {
+    chrome.tabs.create({ url: compareURL })
+    setSuccessMessage("Tab opened!!")
+  }
 
   return (
-    <main className="p-3 gap-2">
+    <main className="p-3 flex flex-col gap-2">
       <div className="flex h-10 gap-1">
         <Input onChange={setBeforeCommitHash} placeholder="fb4494e" />
         <div className="h-full flex items-center">
@@ -29,10 +45,14 @@ const Body = ({ repositoryURL }: bodyProps) => {
         </div>
         <Input onChange={setAfterCommitHash} placeholder="701b762" />
       </div>
-      <div>URL: {compareURL}</div>
-      <div className="flex">
-        <Button />
-        <div>Open</div>
+      <div>{compareURL}</div>
+      <div className="flex gap-2">
+        <Button title="Copy" onClick={compareURLCopyToClipboard} />
+        <Button title="Open" onClick={compareURLOpenToNewTab} />
+        <div className="flex items-center">
+          <p className="text-green-600">{successMessage}</p>
+          <p className="text-red-600">{errorMessage}</p>
+        </div>
       </div>
     </main>
   )
